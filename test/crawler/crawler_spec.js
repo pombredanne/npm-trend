25d7fb015663b00bc9a0c51b16814d8eed5c1db2
crabmc <mc1345932@126.com>
2014-07-18 00:17:06 +0800

    add fake process for testing, add case

--- test/crawler/crawler_spec.js
+++ test/crawler/crawler_spec.js
@@ -1,9 +1,14 @@
 var expect = require("chai").expect;
 var sinon = require("sinon");
+var path = require("path");
+var os = require("os");
+var fs = require("fs");
 var get_crawler = require("../../lib/crawler");
 
 describe("Crawler object", function() {
   var crawler;
+  var fake_cl = path.normalize(__dirname + "/../../lib/crawler/fake/cl.js");
+  var fake_testlog = path.join(os.tmpdir(), "/npm-trend.fakecl.log");
 
   beforeEach(function() {
     crawler = get_crawler();
@@ -21,8 +26,38 @@ describe("Crawler object", function() {
     });
   });
 
-  // describe("launch", function() {
-  //   it("could launch and kill crawler", function() {
-  //   });
-  // });
+  describe("launch, relaunch, kill Crawler process", function() {
+    beforeEach(function() {
+      crawler.config({
+        cwl_file: fake_cl
+      });
+    });
+
+    it("throw error if cannot find crawler process", function() {
+      crawler.config({
+        cwl_file: "./no"
+      });
+
+      expect(function() {
+        crawler.launch();
+      }).to.
+      throw (Error);
+    });
+
+    it("could launch crawler", function(done) {
+      crawler.config({
+        args: ["--shortrun"]
+      });
+
+      expect(function() {
+        crawler.launch();
+      }).to.not.
+      throw ();
+
+      setTimeout(function() {
+        expect(fs.readFileSync(fake_testlog, "utf-8")).to.eql("Fake crawler start\n" + "Fake crawler finish\n");
+        done();
+      }, 1000);
+    });
+  });
 });
